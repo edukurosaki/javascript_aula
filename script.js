37e30ac5871164daf8cd17e9f7f854d290546bc8
Eduardo Francisco <kurosaki.yslan@gmail.com>
2020-11-03 11:07:26 -0300

    Feito as aulas de laços de repetição

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
+
+// Aula de Strings e variaves
+/*
 let nome = 'Eduardo'
 let idade = 42;
 
 alert('Bem vindo a mais uma aula'+ nome);
 document.write('Meu nome e '+nome + 'e tenho '+ idade + 'anos');
+*/
+
+//Aula de aArrays
+/*
+let lista = ['maça', 'pera', 'laranja']
+//document.write(lista)
+console.log(lista.length)
+console.log(lista.toString())
+console.log(lista[1])
+*/
+
+//Aula de condicionais
+
+/*
+let idade = prompt('Digite aqui sua idade: ')
+if (idade>18) {
+    alert('maior de idade pode entrar')
+}else{
+    alert("menor de idade nao podera entrar")
+}
+*/
+
+let cont = 0
+while (cont <= 6) {
+    alert(cont);
+    cont++;
+}
